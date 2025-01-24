4cfe6a993c0cede0297bb9978427ffaf92d54782
sanjidadokmi <sanjidadokmi@gmail.com>
2025-01-25 01:38:58 +0600

    DATA DYNAMIC DONE

--- src/components/CarBox.jsx
+++ src/components/CarBox.jsx
@@ -1,17 +1,34 @@
-const CarBox = () => {
+import PropTypes from "prop-types";
+
+
+const CarBox = ({car} ) => {
+
+  const { title, brand, year, price, isPremium } = car;
+  
   return (
     <>
       <div className="p-4  border-2 rounded  border-gray-300 shadow-sm">
-        <h3 className="text-xl font-bold">Luxury Sedan</h3>
+        <h3 className="text-xl font-bold">{title}</h3>
         <div className="mt-3 ">
-          <p><strong>Brand:</strong> <span>Mercedes</span></p>
-          <p><strong>Year:</strong> <span>2023</span></p>
-          <p><strong>Price:</strong> <span>$80,000</span></p>
-          <p><strong>Premium:</strong> <span>Yes</span></p>
+          <p><strong>Brand:</strong> <span>{brand}</span></p>
+          <p><strong>Year:</strong> <span>{year}</span></p>
+          <p><strong>Price:</strong> <span>{price}</span></p>
+          <p><strong>Premium:</strong> <span>{isPremium ? 'True': 'False'}</span></p>
         </div>
       </div>
     </>
   );
 };
+
+CarBox.propTypes = {
+  
+  car: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    brand: PropTypes.string.isRequired,
+    year: PropTypes.number.isRequired,
+    price: PropTypes.number.isRequired,
+    isPremium: PropTypes.bool.isRequired
+  }).isRequired
+}
 
 export default CarBox;
--- src/components/CarLists.jsx
+++ src/components/CarLists.jsx
@@ -1,14 +1,18 @@
 import CarBox from "./CarBox";
+import cars from '../car.json'; // Import the car.json file
 
 const CarLists = () => {
+  
   return (
     <>
       <div className="grid grid-cols-3 gap-4">
-        <CarBox />
-        <CarBox />
-        <CarBox />
-        <CarBox />
-        <CarBox />
+        {
+          cars.map( (carItem, index) =>{
+           return <CarBox key={index} car={carItem} />            
+          })
+        }
+        
+
       </div>
     </>
   );
